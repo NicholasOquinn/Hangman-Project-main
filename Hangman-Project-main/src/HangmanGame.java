224190a9b35317d30f3dd9ba1dea8d52613105b7
NicholasOquinn <Nicholas.OQuinn08@ed.cna.nl.ca>
2022-08-08 14:05:46 -0230

    Initial commit

---
+++ Hangman-Project-main/src/HangmanGame.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class HangmanGame {
+    private static int count = 0;
+
+
+    public static void main(String[] args) {
+        Scanner userGuess = new Scanner(System.in);
+
+        while (count < 7)
+            System.out.println("Guess a letter in the word!");
+
+            // Nick's test comment for commit purposes
+
+    }
+}
